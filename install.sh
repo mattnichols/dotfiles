5019d33b7e1d68afd10de06559f73d1f759244fc
Matthew Nichols <matt@nichols.name>
2015-08-02 02:17:02 -0600

    Adds already installed check

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+if [ -e "$HOME/.dotfiles" ]; then
+  echo ""
+  echo ""
+  echo "!!! dotfiles already installed"
+  echo ""
+  echo "  To star over run: uninstall_dotfiles"
+  echo ""
+  exit
+fi
+
 pushd "$HOME"
 git clone git@github.com:mattnichols/dotfiles.git "$HOME/.dotfiles"
 pushd "$HOME/.dotfiles"

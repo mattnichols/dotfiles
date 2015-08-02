af95dbdf2ec6d6aa256ac4d0ca1185b5bdebaa1f
Matthew Nichols <matt@nichols.name>
2015-08-01 23:42:37 -0600

    Adds oh my zsh

--- install.sh
+++ install.sh
@@ -1,7 +1,24 @@
 #!/bin/sh
 
-cutstring="DO NOT EDIT BELOW THIS LINE"
 
+if hash brew 2>/dev/null; then
+  echo "Homebrew is installed"
+else
+  echo "Installing Homebrew"
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+echo "Updating brews..."
+brew update
+
+# Switch to Zsh
+brew install zsh
+if ['$SHELL' != '/bin/zsh']; then
+  chsh -s $(which zsh)
+fi
+
+
+# Link to .dotfiles
+cutstring="DO NOT EDIT BELOW THIS LINE"
 for name in *; do
   target="$HOME/.$name"
   if [ -e $target ]; then
@@ -33,3 +50,33 @@ for name in *; do
     fi
   fi
 done
+
+
+# Install Oh My ZSH
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+
+# Configure Oh My Zsh
+sed 's/robbyrussell/peepcode/' "$HOME/.zshrc" > ~/.tmpfile && mv ~/.tmpfile "$HOME/.zshrc"
+sed 's/(git)/(ruby rails zsh_reload sublime bundler common-aliases)/' "$HOME/.zshrc" > ~/.tmpfile && mv ~/.tmpfile "$HOME/.zshrc"
+echo '' >> "$HOME/.zshrc"
+echo '' >> "$HOME/.zshrc"
+echo 'source "$HOME/.aliases"' >> "$HOME/.zshrc"
+# echo 'fpath=("$HOME/.zsh/completion" $fpath)' >> "$HOME/.zshrc"
+# echo 'source "$HOME/.configure"' >> "$HOME/.zshrc"
+#echo 'source "$HOME/.prompt"' >> "$HOME/.zshrc"
+
+# echo '' >> "$HOME/.zshrc"
+# echo 'autoload -U compinit' >> "$HOME/.zshrc"
+# echo 'compinit' >> "$HOME/.zshrc"
+# echo '' >> "$HOME/.zshrc"
+echo 'export EDITOR=subl' >> "$HOME/.zshrc"
+
+
+
+echo Installing RVM...
+brew install gpg
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+curl -sSL https://get.rvm.io | bash -s stable
+
+# Launch!
+source ~/.zshrc
---
+++ uninstall.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+cutstring="DO NOT EDIT BELOW THIS LINE"
+
+for name in *; do
+  target="$HOME/.$name"
+  if [ -e $target ]; then
+    echo "Removing $target"
+    rm $target
+  fi
+done
+
+rm -f $HOME/.profile
+rm -f $HOME/.bash*
+rm -f $HOME/.zsh*
+rm -f $HOME/.zcomp*
+
+rm -Rf $HOME/.rvm
+uninstall_oh_my_zsh
+rm -Rf $HOME/.oh-my-zsh
+
+chsh -s $(which bash)

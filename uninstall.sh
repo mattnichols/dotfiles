f4a935d963f718a0ae8a314a01eb096280f70177
Matthew Nichols <matt@nichols.name>
2015-08-04 00:51:44 -0600

    Trying to fix

--- uninstall.sh
+++ uninstall.sh
@@ -1,22 +1,22 @@
-#!/bin/sh
+#!/bin/zsh
 
 for name in ~/.dotfiles/*; do
   target="$HOME/.$name"
   if [ -e $target ]; then
     echo "Removing $target"
-    rm -f $target
+    rm -f "$target"
   fi
 done
 
-rm -f ~/.profile
-rm -f ~/.bash*
-rm -f ~/.zsh*
-rm -f ~/.zcomp*
+rm -f "$HOME/.profile"
+rm -f "$HOME/.bash*"
+rm -f "$HOME/.zsh*"
+rm -f "$HOME/.zcomp*"
 
-rm -Rf ~/.rvm
+rm -Rf "$HOME/.rvm"
 uninstall_oh_my_zsh
-rm -Rf ~/.oh-my-zsh
-rm -Rf ~/.dotfiles
+rm -Rf "$HOME/.oh-my-zsh"
+rm -Rf "$HOME/.dotfiles"
 
 echo "Relaunch Terminal!!"
 exit

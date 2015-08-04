e5f6a293f56b740f814c4b63478f1ed567c25406
Matthew Nichols <matt@nichols.name>
2015-08-04 01:01:00 -0600

    Fix paths in uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -1,22 +1,22 @@
 #!/bin/zsh
 
-for name in ~/.dotfiles/*; do
+pushd ~/.dotfiles
+for name in *; do
   target="$HOME/.$name"
   if [ -e $target ]; then
-    echo "Removing $target"
+	  echo "Removing $target"
     rm -f "$target"
   fi
 done
+popd
 
 rm -f "$HOME/.profile"
 rm -f "$HOME/.bash*"
 rm -f "$HOME/.zsh*"
 rm -f "$HOME/.zcomp*"
-
 rm -Rf "$HOME/.rvm"
 uninstall_oh_my_zsh
 rm -Rf "$HOME/.oh-my-zsh"
 rm -Rf "$HOME/.dotfiles"
-
-echo "Relaunch Terminal!!"
+"Relaunch Terminal!!"
 exit

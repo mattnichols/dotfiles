6a9277f98da3490307f7429e88b0ae01b34b21ad
matthew-nichols <matthew.nichols@mx.com>
2015-08-26 16:00:33 -0600

    Adds ipaddr function.

--- zsh_custom/plugins/light-utils/light-utils.plugin.zsh
+++ zsh_custom/plugins/light-utils/light-utils.plugin.zsh
@@ -35,6 +35,12 @@ alias %=' '
 alias flushdns='dscacheutil -flushcache'
 alias add_dock_space="defaults write com.apple.dock persistent-apps -array-add '{"tile-type"="spacer-tile";}' && killall Dock"
 alias uninstall_dotfiles="source ~/.dotfiles/tools/uninstall.sh"
+function ipaddr() {
+  ifconfig | grep -v "inet 127" | grep "inet \([0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\)" | awk -F " " '{print $2}'
+}
+
+
+
 
 # Process controls
 # Setup

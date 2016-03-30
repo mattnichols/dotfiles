df2c2e5d55bac0b7aa8f9c978474f9ac88f4a9ee
matthew-nichols <matthew.nichols@mx.com>
2016-03-30 15:59:51 -0600

    Add cobalt2 theme

--- tools/setup.sh
+++ tools/setup.sh
@@ -75,6 +75,8 @@ npm install --global trash
 brew install z
 brew install zsh
 
+apm install cobalt2-syntax
+
 echo "Installing RVM..."
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable

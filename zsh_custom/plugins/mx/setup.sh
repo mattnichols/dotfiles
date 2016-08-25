ad11929b651b5efe57cd776360ea8d4384071a26
matthew-nichols <matthew.nichols@mx.com>
2016-08-25 14:52:39 -0600

    Some setup fixes

--- zsh_custom/plugins/mx/setup.sh
+++ zsh_custom/plugins/mx/setup.sh
@@ -9,15 +9,23 @@ brew install redis
 ln -sfv /usr/local/opt/redis/*.plist ~/Library/LaunchAgents
 launchctl load ~/Library/LaunchAgents/homebrew.mxcl.redis.plist
 
+chown -R `whoami` /usr/local
+chown -R `whoami` /var
+
 brew install postgresql
 rm -rf /usr/local/var/postgres && initdb /usr/local/var/postgres
 mkdir /var/pgsql_socket/
 ln -s /private/tmp/.s.PGSQL.5432 /var/pgsql_socket/
 ln -sfv /usr/local/opt/postgresql/*.plist ~/Library/LaunchAgents
 launchctl load ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist
-pause 10
-createuser -s postgres
 
+pause 10
+
+# Cleanup and re-init database
+rm -R /usr/local/var/postgres
+initdb /usr/local/var/postgres -E utf8
+psql postgres -c 'CREATE EXTENSION "adminpack";' # Not sure I need this...
+createuser -s postgres
 
 brew install rabbitmq
 ln -sfv /usr/local/opt/rabbitmq/*.plist ~/Library/LaunchAgents
@@ -28,6 +36,7 @@ brew install protobuf
 brew install imagemagick
 brew install ansible
 brew install ant
+brew install tmux
 
 # This once solved an issue running batcave locally. Doesn't seem to work anymore.
 # brew uninstall zeromq

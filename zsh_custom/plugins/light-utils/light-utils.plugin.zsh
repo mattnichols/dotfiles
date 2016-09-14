1ec184770095992e56108a99fd2010b742660b5d
matthew-nichols <matthew.nichols@mx.com>
2016-09-14 11:42:01 -0600

    Add jdkswitch command

--- zsh_custom/plugins/light-utils/light-utils.plugin.zsh
+++ zsh_custom/plugins/light-utils/light-utils.plugin.zsh
@@ -53,3 +53,19 @@ alias dk='docker'
 
 alias postgresql_kill='launchctl unload ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist&&sleep 5&&rm /usr/local/var/postgres/postmaster.pid'
 alias postgresql_start='launchctl load ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist'
+
+# https://www.jayway.com/2014/01/15/how-to-switch-jdk-version-on-mac-os-x-maverick/
+function setjdk() {
+  if [ $# -ne 0 ]; then
+    removeFromPath '/System/Library/Frameworks/JavaVM.framework/Home/bin'
+    if [ -n "${JAVA_HOME+x}" ]; then
+      removeFromPath $JAVA_HOME
+    fi
+    export JAVA_HOME=`/usr/libexec/java_home -v $@`
+    export PATH=$JAVA_HOME/bin:$PATH
+  fi
+}
+
+function removeFromPath() {
+  export PATH=$(echo $PATH | sed -E -e "s;:$1;;" -e "s;$1:?;;")
+}

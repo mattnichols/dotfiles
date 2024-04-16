34fffb75d6ec38ebb0247d294d303005bb7d309b
Matt Nichols <Matthew.Nichols@mx.com>
2024-04-16 14:09:19 -0600

    Fix typo

--- .config/kickstart/init.lua
+++ .config/kickstart/init.lua
@@ -4,7 +4,7 @@
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
-vaim.g.maplocalleader = ' '
+vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed
 vim.g.have_nerd_font = false
